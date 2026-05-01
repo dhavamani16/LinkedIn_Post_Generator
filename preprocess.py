dcff086110eb774b70d6063bb2d318de64c08775
Dhavamani A <dhavamani17092005@gmail.com>
2026-05-01 08:56:18 +0530

    Refactor post enrichment process in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -12,7 +12,6 @@ def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"
             metadata = extract_metadata(post['text'])
             post_with_metadata = post | metadata
             enriched_posts.append(post_with_metadata)
-
     unified_tags = get_unified_tags(enriched_posts)
     for post in enriched_posts:
         current_tags = post['tags']
